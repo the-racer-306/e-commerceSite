14a3b967ea781e1e105771e80ba904cd7ff62ccb
= <khaled@gmail.com>
2022-11-24 23:54:53 +0200

    create react app with vite -> push hompage files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Homepage from "./components/hompage/Homepage.component";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Homepage h1="Hats" />
+      {/* <Homepage h1="Shoes" />
+      <Homepage h1="Women" />
+      <Homepage h1="Men" /> */}
+    </div>
+  );
+}
+
+export default App;
