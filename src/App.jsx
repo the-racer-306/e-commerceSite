8f843894dbf7c1f850cdcec358c30c5b7b280b82
= <khaled@gmail.com>
2022-11-25 16:45:00 +0200

    broken our homepage into smaller peices, finished styling our menu-item

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import Homepage from "./components/hompage/Homepage.component";
+import Homepage from "./pages/hompage/Homepage.component";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <Homepage h1="Hats" />
-      {/* <Homepage h1="Shoes" />
-      <Homepage h1="Women" />
-      <Homepage h1="Men" /> */}
+      <Homepage />
     </div>
   );
 }
