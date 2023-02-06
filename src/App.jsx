d698c819c25edafcd424f4fb2af4ad8d7f4b5976
the-racer-306 <khaledmamdouh306@gmail.com>
2023-02-06 16:28:41 +0200

    update our menu-items to navigate withRouter Hoc

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 import React from "react";
-// import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import "./App.css";
-import SignInSignUp from "./pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component";
-// import Homepage from "./pages/hompage/Homepage.component";
+import HomePage from "./pages/hompage/Homepage.component";
+// import SignInSignUp from "./pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component";
 // import ShopPage from "./pages/shop/shop.component";
+
+const HatsPage = () => {
+	const navigate = useNavigate();
+	return (
+		<>
+			<div>Hats Page</div>
+			<button
+				onClick={() => {
+					navigate("/");
+				}}
+			>
+				go to main Page
+			</button>
+		</>
+	);
+};
 
 function App() {
 	return (
 		<div>
-			<SignInSignUp />
+			<Routes>
+				<Route exact path="/" element={<HomePage />} />
+				<Route path="/hats" element={<HatsPage />} />
+			</Routes>
 		</div>
 	);
 }
 
 export default App;
-
-{
-	/* <Routes>
-	<Route exact path="/" component={Homepage} />
-	<Route path="/shop" component={ShopPage} />
-</Routes> */
-}
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,23 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
-import { useNavigate } from "react-router";
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
-	const navigate = useNavigate();
-	<div className={`${size} menu-item`} onClick={() => navigate(linkUrl)}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+	<div
+		className={`${size} menu-item`}
+		onClick={() => history.push(`${match.url}${linkUrl}`)}
+	>
 		<div
-			style={{ backgroundImage: `url(${imageUrl})` }}
 			className="background-image"
+			style={{
+				backgroundImage: `url(${imageUrl})`,
+			}}
 		/>
 		<div className="content">
 			<h1 className="title">{title.toUpperCase()}</h1>
 			<span className="subtitle">Shop Now</span>
 		</div>
-	</div>;
-};
+	</div>
+);
 
-export default MenuItem;
+export default withRouter(MenuItem);
