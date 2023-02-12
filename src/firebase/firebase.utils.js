0cc0c0c5ee1bf4c65a09ce0a4bced9babb35379f
the-racer-306 <khaledmamdouh306@gmail.com>
2023-02-12 23:12:41 +0200

    updated our firestore features

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,30 @@ const config = {
 	measurementId: "G-HFTQH0NWHQ",
 };
 
+export const createUserProfileDocument = async (userAuth, addtionalData) => {
+	if (!userAuth) return;
+
+	const userRef = firestore.doc("users/1324kjskdfj");
+
+	const snapShot = await userRef.get();
+
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...addtionalData,
+			});
+		} catch (error) {
+			console.log("error created user", error.message);
+		}
+	}
+	return userRef;
+};
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
