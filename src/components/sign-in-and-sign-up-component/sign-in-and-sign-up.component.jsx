6a5f5a866bb6f7959d7cb2ffbdb9c9e9e7cd1e7c
the-racer-306 <khaledmamdouh306@gmail.com>
2023-02-14 01:05:17 +0200

    sign up with your email and password

--- src/components/sign-in-and-sign-up-component/sign-in-and-sign-up.component.jsx
+++ src/components/sign-in-and-sign-up-component/sign-in-and-sign-up.component.jsx
@@ -3,10 +3,11 @@ import React, { Component } from "react";
 import "./sign-in-and-sign-up.styles.scss";
 
 import SignIn from "../sign-in/sign-in.component";
-
+import SignUP from "../sign-up/sign-up.component";
 const SignInAndSignUpPage = () => (
-	<div>
+	<div className="sign-in-up-page">
 		<SignIn />
+		<SignUP />
 	</div>
 );
 
